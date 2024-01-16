d98d7dcc469c948d4c6229b08045fafa9e92f87c
daniheck-msft <118814529+daniheck-msft@users.noreply.github.com>
2024-01-16 14:52:34 +0100

    initial commit

---
+++ azcli-ps/03_create_webapp.ps1
@@ -0,0 +1,43 @@
+$rg="bc-AppServices"
+$region = "eastus"
+$webappname = "myPlayWebApp"
+$appplanname = "playAppPlan"
+
+#########################################
+# Sample Get App 
+#########################################
+git clone https://github.com/Azure-Samples/html-docs-hello-world.git
+cd html-docs-hello-world
+
+#########################################
+# Create RG 
+#########################################
+az group create --location $region --resource-group $rg
+
+#########################################
+# Deploy Web App including AppPlan
+#########################################
+az webapp up --location $region --name $webappname --resource-group $rg --plan $appplanname --html --sku F1 -b
+
+#show url of the webapp
+$url = az webapp show --name $webappname --resource-group $rg --query "defaultHostName" --output tsv
+#show webapp in chrome
+start chrome $url
+
+#update webapp
+az webapp up --location $region --name $webappname --resource-group $rg --plan $appplanname --html --sku F1 -b
+
+#########################################
+# Delete Web App including AppPlan
+#########################################
+az webapp delete --name $webappname --resource-group $rg 
+az appservice plan delete --resource-group $rg --name $appplanname --yes
+
+###############################
+# List resources in RG
+###############################
+az resource list --resource-group $rg --output table
+
+# Documentation: https://docs.microsoft.com/en-us/azure/app-service/
+# Pricing: https://azure.microsoft.com/en-us/pricing/details/app-service/
+
---
+++ azcli-ps/04_deploy_github.ps1
@@ -0,0 +1,21 @@
+$rg="bc-AppServices"
+$region = "eastus"
+$webapp = "WebAppFromGitHub"
+$appServicePlan = "playAppPlan"
+$tag="deploy-github.sh"
+$gitrepo="https://github.com/Azure-Samples/php-docs-hello-world" # Replace the following URL with your own public GitHub repo URL if you have one
+
+# Create an App Service plan in `FREE` tier.
+# az appservice plan create --name $appServicePlan --resource-group $resourceGroup --sku FREE
+
+# Create a web app.
+az webapp create --name $webapp --resource-group $rg --plan $appServicePlan
+
+# Deploy code from a public GitHub repository. 
+az webapp deployment source config --name $webapp --resource-group $rg --repo-url $gitrepo --branch master --manual-integration
+
+$url = az webapp show --name $webapp --resource-group $rg --query "defaultHostName" --output tsv
+echo $url
+#show webapp in chrome
+start chrome $url
+
